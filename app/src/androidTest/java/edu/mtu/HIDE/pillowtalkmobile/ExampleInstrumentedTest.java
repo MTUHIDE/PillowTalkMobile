8e7d5ad83e6cc7d4d4934041fa56f7c0a3188f02
Javen Zamojcin <javen.william@gmail.com>
2021-01-25 19:37:49 -0500

    create unit test

--- app/src/androidTest/java/edu/mtu/HIDE/pillowtalkmobile/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/edu/mtu/HIDE/pillowtalkmobile/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package edu.mtu.HIDE.pillowtalkmobile;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
