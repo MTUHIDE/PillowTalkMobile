8e61bc480e3bb35a74b1fe9759cc3930bac228ac
Josh Overbeek <joshoverbeek0@gmail.com>
2021-10-19 23:40:51 -0400

    Deleted unused code snippet

--- app/src/main/java/edu/mtu/HIDE/pillowtalkmobile/SettingsActivity.java
+++ app/src/main/java/edu/mtu/HIDE/pillowtalkmobile/SettingsActivity.java
@@ -129,11 +129,4 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
     }
-
-    public static class SettingsFragment extends PreferenceFragmentCompat {
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            //setPreferencesFromResource(R.xml.root_preferences, rootKey);
-        }
-    }
 }
